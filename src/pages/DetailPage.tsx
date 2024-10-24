47745ab29d8bb6240b5c33e4bc723d8aa2b177fc
Tirth Patel <tirthp292003@gmail.com>
2024-10-24 18:18:49 +0530

    Modify DetailPage

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,87 +1,87 @@
-// import { useGetRestaurant } from "@/api/RestaurantApi";
-// import MenuItem from "@/components/MenuItemComponent";
-// import OrderSummary from "@/components/OrderSummary";
-// import RestaurantInfo from "@/components/RestaurantInfo";
-// import { AspectRatio } from "@/components/ui/aspect-ratio";
-// import { Card } from "@/components/ui/card";
-// import { MenuItem as MenuItemType } from "@/types";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useGetRestaurant } from "@/api/RestaurantApi";
+import MenuItem from "@/components/MenuItemComponent";
+import OrderSummary from "@/components/OrderSummary";
+import RestaurantInfo from "@/components/RestaurantInfo";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-// export type CartItem = {
-//   _id: string;
-//   name: string;
-//   price: number;
-//   quantity: number;
-// };
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
-// const DetailPage = () => {
-//   const { restaurantId } = useParams();
-//   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+const DetailPage = () => {
+  const { restaurantId } = useParams();
+  const { restaurant, isLoading } = useGetRestaurant(restaurantId);
 
-//   const [cartItems, setCartItems] = useState<CartItem[]>([]);
-//   const addToCart = (menuItem: MenuItemType) => {
-//     setCartItems((prevCartItems) => {
-//       const existingCartItem = prevCartItems.find(
-//         (cartItem) => cartItem._id === menuItem._id
-//       );
-//       let updatedCartItems;
-//       if (existingCartItem) {
-//         updatedCartItems = prevCartItems.map((cartItem) =>
-//           cartItem._id === menuItem._id
-//             ? { ...cartItem, quantity: cartItem.quantity + 1 }
-//             : cartItem
-//         );
-//       } else {
-//         updatedCartItems = [
-//           ...prevCartItems,
-//           {
-//             _id: menuItem._id,
-//             name: menuItem.name,
-//             price: menuItem.price,
-//             quantity: 1,
-//           },
-//         ];
-//       }
-//       return updatedCartItems;
-//     });
-//   };
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItems((prevCartItems) => {
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+      let updatedCartItems;
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+      return updatedCartItems;
+    });
+  };
 
-//   if (isLoading || !restaurant) {
-//     return "Loading.....";
-//   }
+  if (isLoading || !restaurant) {
+    return "Loading.....";
+  }
 
-//   return (
-//     <div className="flex flex-col gap-10">
-//       <AspectRatio ratio={16 / 5}>
-//         <img
-//           src={restaurant.imageUrl}
-//           className="rounded-md obeject-cover h-full w-full -mt-8"
-//         />
-//       </AspectRatio>
-//       <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32 -mt-12">
-//         <div className="flex flex-col gap-4">
-//           <RestaurantInfo restaurant={restaurant} />
-//           <span className="text-2xl font-bold tracking-tight ml-1">Menu</span>
-//           {restaurant.menuItems.map((menuItem) => (
-//             <MenuItem
-//               menuItem={menuItem}
-//               addToCart={() => addToCart(menuItem)}
-//             />
-//           ))}
-//         </div>
+  return (
+    <div className="flex flex-col gap-10">
+      <AspectRatio ratio={16 / 5}>
+        <img
+          src={restaurant.imageUrl}
+          className="rounded-md obeject-cover h-full w-full "
+        />
+      </AspectRatio>
+      <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32 ">
+        <div className="flex flex-col gap-4">
+          <RestaurantInfo restaurant={restaurant} />
+          <span className="text-2xl font-bold tracking-tight ml-1">Menu</span>
+          {restaurant.menuItems.map((menuItem) => (
+            <MenuItem
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
+          ))}
+        </div>
 
-//         <div>
-//           <Card>
-//             <OrderSummary restaurant={restaurant} cartItems={cartItems} />
-//           </Card>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+        <div>
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default DetailPage;
+export default DetailPage;
 
 // // import { useGetRestaurant } from "@/api/RestaurantApi";
 // // import MenuItem from "@/components/MenuItemComponent";
@@ -177,101 +177,101 @@
 
 // // export default DetailPage;
 
-import { useGetRestaurant } from "@/api/RestaurantApi";
-import MenuItem from "@/components/MenuItemComponent";
-import OrderSummary from "@/components/OrderSummary";
-import RestaurantInfo from "@/components/RestaurantInfo";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { Card } from "@/components/ui/card";
-import { MenuItem as MenuItemType } from "@/types";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// import { useGetRestaurant } from "@/api/RestaurantApi";
+// import MenuItem from "@/components/MenuItemComponent";
+// import OrderSummary from "@/components/OrderSummary";
+// import RestaurantInfo from "@/components/RestaurantInfo";
+// import { AspectRatio } from "@/components/ui/aspect-ratio";
+// import { Card } from "@/components/ui/card";
+// import { MenuItem as MenuItemType } from "@/types";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 
-export type CartItem = {
-  _id: string;
-  name: string;
-  price: number;
-  quantity: number;
-};
+// export type CartItem = {
+//   _id: string;
+//   name: string;
+//   price: number;
+//   quantity: number;
+// };
 
-const DetailPage = () => {
-  const { restaurantId } = useParams();
-  const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+// const DetailPage = () => {
+//   const { restaurantId } = useParams();
+//   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
 
-  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
-    // Load cart items from localStorage on initial render
-    const savedCartItems = localStorage.getItem("cartItems");
-    return savedCartItems ? JSON.parse(savedCartItems) : [];
-  });
+//   const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+//     // Load cart items from localStorage on initial render
+//     const savedCartItems = localStorage.getItem("cartItems");
+//     return savedCartItems ? JSON.parse(savedCartItems) : [];
+//   });
 
-  const addToCart = (menuItem: MenuItemType) => {
-    setCartItems((prevCartItems) => {
-      const existingCartItem = prevCartItems.find(
-        (cartItem) => cartItem._id === menuItem._id
-      );
-      let updatedCartItems;
-      if (existingCartItem) {
-        updatedCartItems = prevCartItems.map((cartItem) =>
-          cartItem._id === menuItem._id
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        );
-      } else {
-        updatedCartItems = [
-          ...prevCartItems,
-          {
-            _id: menuItem._id,
-            name: menuItem.name,
-            price: menuItem.price,
-            quantity: 1,
-          },
-        ];
-      }
-      // Update localStorage with the new cart items
-      localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-      return updatedCartItems;
-    });
-  };
+//   const addToCart = (menuItem: MenuItemType) => {
+//     setCartItems((prevCartItems) => {
+//       const existingCartItem = prevCartItems.find(
+//         (cartItem) => cartItem._id === menuItem._id
+//       );
+//       let updatedCartItems;
+//       if (existingCartItem) {
+//         updatedCartItems = prevCartItems.map((cartItem) =>
+//           cartItem._id === menuItem._id
+//             ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//             : cartItem
+//         );
+//       } else {
+//         updatedCartItems = [
+//           ...prevCartItems,
+//           {
+//             _id: menuItem._id,
+//             name: menuItem.name,
+//             price: menuItem.price,
+//             quantity: 1,
+//           },
+//         ];
+//       }
+//       // Update localStorage with the new cart items
+//       localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+//       return updatedCartItems;
+//     });
+//   };
 
-  useEffect(() => {
-    // Update localStorage whenever cartItems changes
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartItems]);
+//   useEffect(() => {
+//     // Update localStorage whenever cartItems changes
+//     localStorage.setItem("cartItems", JSON.stringify(cartItems));
+//   }, [cartItems]);
 
-  if (isLoading || !restaurant) {
-    return "Loading.....";
-  }
+//   if (isLoading || !restaurant) {
+//     return "Loading.....";
+//   }
 
-  return (
-    <div className="flex flex-col gap-10">
-      <AspectRatio ratio={16 / 5}>
-        <img
-          src={restaurant.imageUrl}
-          className="rounded-md object-cover h-full w-full -mt-8"
-          // alt={restaurant.name}
-        />
-      </AspectRatio>
-      <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32 -mt-12">
-        <div className="flex flex-col gap-4">
-          <RestaurantInfo restaurant={restaurant} />
-          <span className="text-2xl font-bold tracking-tight ml-1">Menu</span>
-          {restaurant.menuItems.map((menuItem) => (
-            <MenuItem
-              key={menuItem._id}
-              menuItem={menuItem}
-              addToCart={() => addToCart(menuItem)}
-            />
-          ))}
-        </div>
+//   return (
+//     <div className="flex flex-col gap-10">
+//       <AspectRatio ratio={16 / 5}>
+//         <img
+//           src={restaurant.imageUrl}
+//           className="rounded-md object-cover h-full w-full "
+//           // alt={restaurant.name}
+//         />
+//       </AspectRatio>
+//       <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32 ">
+//         <div className="flex flex-col gap-4">
+//           <RestaurantInfo restaurant={restaurant} />
+//           <span className="text-2xl font-bold tracking-tight ml-1">Menu</span>
+//           {restaurant.menuItems.map((menuItem) => (
+//             <MenuItem
+//               key={menuItem._id}
+//               menuItem={menuItem}
+//               addToCart={() => addToCart(menuItem)}
+//             />
+//           ))}
+//         </div>
 
-        <div>
-          <Card>
-            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
-          </Card>
-        </div>
-      </div>
-    </div>
-  );
-};
+//         <div>
+//           <Card>
+//             <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+//           </Card>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default DetailPage;
+// export default DetailPage;
